b1d63b82a1557c51ab41cf0f00dbcd106d94a886
Maikel Chandika <mkdika@gmail.com>
2018-11-02 18:59:22 +0700

    Fix and test auto like bot.

--- src/autolike-with-igapi-python.py
+++ src/autolike-with-igapi-python.py
@@ -39,54 +39,53 @@ print('Okay! Checking {0} posts.'.format(
     TARGET['username']
 ))
 
-while(True):
-    print(">> Refresh user feed at {0}".format(
-        time.strftime("%d-%m-%Y %H:%M", time.localtime())))
-    api.getUserFeed(str(TARGET['user_data']['pk']))
+print(">> Fetch user feed at {0}".format(
+    time.strftime("%d-%m-%Y %H:%M", time.localtime())))
+api.getUserFeed(str(TARGET['user_data']['pk']))
 
-    media = TARGET['media'] = api.LastJson['items']
+media = TARGET['media'] = api.LastJson['items']
 
-    print('Total Post @{0}: {1} posts.'.format(
-        TARGET['username'],
-        len(TARGET['media'])
-    ))
+print('Total Post @{0}: {1} posts.'.format(
+    TARGET['username'],
+    len(TARGET['media'])
+))
 
-    for item in media:
-        # print all payload from new feeds
-        # pp.pprint(item)
-        print("======================================================")
-        if item['has_liked']:
-            print("Post ID {0} has been liked.".format(item['id']))
-        else:
-            ts = int(int(item['device_timestamp'])/1000000)
-            local_time = time.localtime(ts)
-            print("Post ID           : {0}".format(item['id']))
-            print("Post Time (Local) : {0}".format(
-                time.strftime("%d-%m-%Y %H:%M", local_time)))
-            print("Like Post         : {0}".format(item['has_liked']))
-            print("Total like count  : {0}".format(item['like_count']))
-
-            if 'carousel_media' in item:
-                list_img = item['carousel_media']
-                print("Carousel Img URL  :")
-                for img in list_img:
-                    print("\t*  {0}".format(
-                        img['image_versions2']['candidates'][1]['url']))
-            else:
-                print("Sample Image URL  :\n\t*  {0}".format(
-                    item['image_versions2']['candidates'][1]['url']))
-
-            print("Post caption      : {0}".format(
-                item['caption']['text'] or 'No caption!'))
-            hasLiked = api.like(item['id'])
-            if hasLiked:
-                print(">> You just liked this post at {0}".format(
-                    time.strftime("%d-%m-%Y %H:%M", time.localtime())))
-                TARGET['media_like'].append(item['id'])
-                count = count + 1
-                print(">> Total auto post like : {0}".format(count))
-                time.sleep(randint(70, 100))
-
-    # to delay the next query, in order to prevent from banned.
-    time.sleep(randint(350, 400))
+for item in media:
+    # print all payload from new feeds
+    # pp.pprint(item)
     print("======================================================")
+    if item['has_liked']:
+        print("Post ID {0} has been liked.".format(item['id']))
+    else:
+        ts = int(int(item['device_timestamp'])/1000000)
+        local_time = time.localtime(ts)
+        print("Post ID           : {0}".format(item['id']))
+        print("Post Time (Local) : {0}".format(
+            time.strftime("%d-%m-%Y %H:%M", local_time)))
+        print("Like Post         : {0}".format(item['has_liked']))
+        print("Total like count  : {0}".format(item['like_count']))
+
+        if 'carousel_media' in item:
+            list_img = item['carousel_media']
+            print("Carousel Img URL  :")
+            for img in list_img:
+                print(
+                    "\t*  {0}".format(img['image_versions2']['candidates'][1]['url']))
+        else:
+            print(
+                "Sample Image URL  :\n\t*  {0}".format(item['image_versions2']['candidates'][1]['url']))
+
+        print("Post caption      : {0}".format(
+            item['caption']['text'] or 'No caption!'))
+        hasLiked = api.like(item['id'])
+        if hasLiked:
+            print(">> You just liked this post at {0}".format(
+                time.strftime("%d-%m-%Y %H:%M", time.localtime())))
+            TARGET['media_like'].append(item['id'])
+            count = count + 1
+            print(">> Total auto post like : {0}".format(count))
+            rnd = randint(10, 25)
+            print(">> Delay {0} secs for next post to like.".format(rnd))
+            time.sleep(rnd)
+
+print("================================================== END")
