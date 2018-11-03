a8042eacdb9b15c353a0e76323afed15635e036d
Maikel Chandika <mkdika@gmail.com>
2018-11-03 09:12:31 +0700

    Update auto like to support multiple account by external YAML config file.

--- src/auto-like-post.py
+++ src/auto-like-post.py
@@ -3,89 +3,138 @@ from pprint import PrettyPrinter
 from getpass import getpass
 from random import randint
 from datetime import datetime
-import time
 
+import time
+import yaml
+import os
+
+
+pp = PrettyPrinter(indent=2)
 count = 0
+
+d1 = int(time.time())
 
 print("=================")
 print("I N S T A G R A M")
 print(" Auto Like Post! ")
 print("=================")
 
-pp = PrettyPrinter(indent=2)
+yaml_config_file_path = os.path.join(os.path.abspath(
+    os.path.dirname(__file__)), 'maxigbot.yml')
 
-print("Please input your IG Account:")
-api = IG(
-    input('Username: '),
-    getpass('Password: ')
-)
+bot_config = yaml.load(open(yaml_config_file_path))
 
-api.login()
+if len(bot_config) > 0:
+    print(">> Configuration file loaded.\n")
 
-TARGET = {
-    'username': '',
-    'media': '',
-    'media_like': [],
-}
+list_user = bot_config['maxigbot']['auto-like-post']['account']
+print(">> Total IG user to process: {0}".format(len(list_user)))
 
-TARGET['username'] = input('Target IG Username: ')
+fin_user = 0
 
-api.searchUsername(TARGET['username'])
+for user in list_user:
 
-TARGET['user_data'] = api.LastJson['user']
+    api = IG(user['username'], user['password'])
+    api.login()
+    print(">> IG user '{0}' logged!".format(user['username']))
+    print(">> Total target user: {0}".format(len(user['target-user'])))
 
-# pp.pprint(TARGET['user_data'])
-print('Okay! Checking {0} posts.'.format(
-    TARGET['username']
-))
+    tot_target = len(user['target-user'])
+    fin_target = 0
 
-print(">> Fetch user feed at {0}".format(
-    time.strftime("%d-%m-%Y %H:%M", time.localtime())))
-api.getUserFeed(str(TARGET['user_data']['pk']))
+    for t_user in user['target-user']:
+        print(">> Now targeting user '{0}'.".format(t_user))
+        TARGET = {
+            'username': '',
+            'media': '',
+            'media_like': [],
+        }
 
-media = TARGET['media'] = api.LastJson['items']
+        TARGET['username'] = t_user
 
-print('Total Post @{0}: {1} posts.'.format(
-    TARGET['username'],
-    len(TARGET['media'])
-))
+        api.searchUsername(TARGET['username'])
 
-for item in media:
-    # print all payload from new feeds
-    # pp.pprint(item)
-    print("======================================================")
-    if item['has_liked']:
-        print("Post ID {0} has been liked.".format(item['id']))
-    else:
-        ts = int(int(item['device_timestamp'])/1000000)
-        local_time = time.localtime(ts)
-        print("Post ID           : {0}".format(item['id']))
-        print("Post Time (Local) : {0}".format(
-            time.strftime("%d-%m-%Y %H:%M", local_time)))
-        print("Like Post         : {0}".format(item['has_liked']))
-        print("Total like count  : {0}".format(item['like_count']))
+        TARGET['user_data'] = api.LastJson['user']
 
-        if 'carousel_media' in item:
-            list_img = item['carousel_media']
-            print("Carousel Img URL  :")
-            for img in list_img:
-                print(
-                    "\t*  {0}".format(img['image_versions2']['candidates'][1]['url']))
+        print(">> Checking '{0}' posts.".format(
+            TARGET['username']
+        ))
+
+        print(">> Fetch '{1}' feed at {0}".format(
+            time.strftime("%d-%m-%Y %H:%M", time.localtime()),
+            t_user))
+        api.getUserFeed(str(TARGET['user_data']['pk']))
+
+        media = TARGET['media'] = api.LastJson['items']
+
+        print(">> Total '{0}' post: {1} posts.".format(
+            TARGET['username'],
+            len(TARGET['media'])
+        ))
+
+        for item in media:
+            # print all payload from new feeds
+            # pp.pprint(item)
+            print("===========================================================")
+            if item['has_liked']:
+                print("User {1} with Post ID {0} has been liked.".format(
+                    item['id'], t_user))
+            else:
+                ts = int(int(item['device_timestamp'])/1000000)
+                local_time = time.localtime(ts)
+                print("User ID           : {0}".format(t_user))
+                print("Post ID           : {0}".format(item['id']))
+                print("Post Time (Local) : {0}".format(
+                    time.strftime("%d-%m-%Y %H:%M", local_time)))
+                print("Like Post         : {0}".format(item['has_liked']))
+                print("Total like count  : {0}".format(item['like_count']))
+
+                if 'carousel_media' in item:
+                    list_img = item['carousel_media']
+                    print("Carousel Img URL  :")
+                    for img in list_img:
+                        print(
+                            "\t*  {0}".format(img['image_versions2']['candidates'][1]['url']))
+                else:
+                    print(
+                        "Sample Image URL  :\n\t*  {0}".format(item['image_versions2']['candidates'][1]['url']))
+
+                print("Post caption      : {0}".format(
+                    item['caption']['text'] or 'No caption!'))
+                hasLiked = api.like(item['id'])
+                if hasLiked:
+                    print(">> You just liked this post at {0}".format(
+                        time.strftime("%d-%m-%Y %H:%M", time.localtime())))
+                    TARGET['media_like'].append(item['id'])
+                    count = count + 1
+                    print(">> Total auto post like : {0}".format(count))
+                    rnd = randint(10, 20)
+                    print(
+                        ">> Delay {0} secs for next post to like.".format(rnd))
+                    time.sleep(rnd)
+
+        print(">> Process '{1}' to target user '{0}' done!".format(
+            t_user, user['username']))
+        fin_target = fin_target + 1
+
+        if fin_target < tot_target:
+            wait_next_target = randint(20, 30)
+            print(">> Wait {0} secs for next target user.\n".format(
+                wait_next_target))
+            time.sleep(wait_next_target)
         else:
-            print(
-                "Sample Image URL  :\n\t*  {0}".format(item['image_versions2']['candidates'][1]['url']))
+            print(">> Done all target user for '{0}'.".format(
+                user['username']))
 
-        print("Post caption      : {0}".format(
-            item['caption']['text'] or 'No caption!'))
-        hasLiked = api.like(item['id'])
-        if hasLiked:
-            print(">> You just liked this post at {0}".format(
-                time.strftime("%d-%m-%Y %H:%M", time.localtime())))
-            TARGET['media_like'].append(item['id'])
-            count = count + 1
-            print(">> Total auto post like : {0}".format(count))
-            rnd = randint(10, 25)
-            print(">> Delay {0} secs for next post to like.".format(rnd))
-            time.sleep(rnd)
+    fin_user = fin_user + 1
 
-print("================================================== END")
+    if fin_user < len(list_user):
+        wait_next_user = randint(30, 45)
+        print(">> Wait {0} secs for next IG user process.\n".format(
+            wait_next_user))
+        time.sleep(wait_next_user)
+    else:
+        print("======================================================= END")
+
+d2 = int(time.time())
+print("All done in {0} secs".format(d2-d1))
